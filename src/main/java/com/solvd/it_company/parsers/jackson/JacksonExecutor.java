13753c4550d51a8ad09a193e73c05a9d85cc41ef
AlekseiDubynskyi <dubynskyialeksei@gmail.com>
2022-09-22 20:14:02 +0300

    Add Jackson

--- src/main/java/com/solvd/it_company/parsers/jackson/JacksonExecutor.java
+++ src/main/java/com/solvd/it_company/parsers/jackson/JacksonExecutor.java
@@ -11,7 +11,6 @@ public class JacksonExecutor {
     private static final Logger LOGGER = LogManager.getLogger(JacksonExecutor.class);
 
     public static void main(String[] args) throws IOException {
-
         writeJackson();
         readJackson();
     }
